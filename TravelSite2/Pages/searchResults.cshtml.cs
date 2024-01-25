7bbff01a33ce79a1ede2b8fb46f15aaf6cc27d99
Joshua Meadows <jtmeadows@mail.wvu.edu>
2024-01-25 09:04:17 -0500

    Add project files.

---
+++ TravelSite2/Pages/searchResults.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TravelSite2.Pages
+{
+    public class searchResultsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
