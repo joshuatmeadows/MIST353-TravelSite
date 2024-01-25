09c3241dda72d29c16aa23a9a30a0c6fbce7b334
Joshua Meadows <jtmeadows@mail.wvu.edu>
2024-01-25 15:40:42 -0500

    Code from class 1/25

--- TravelSite2/wwwroot/js/site.js
+++ TravelSite2/wwwroot/js/site.js
@@ -2,3 +2,43 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)
+    }
+    else {
+        alert("Not supported")
+    }
+}
+
+function showPosition(position) {
+    document.getElementById("latTxt").value = position.coords.latitude;
+    document.getElementById("longTxt").value = position.coords.longitude;
+    document.getElementById("results").innerHTML = position.coords.latitude + ', ' + position.coords.longitude;
+    getAddress(position.coords.latitude, position.coords.longitude)
+}
+
+async function getAddress(lat, long) {
+    const response = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${long}&format=json`);
+    const data = await response.json();
+    if (data.address) {
+        displayAddress(data.address);
+    }
+}
+
+function displayAddress(address) {
+    if (address["city"]) {
+        document.getElementById("locInp").value = address["city"] + ', ' + address["state"];
+
+    }
+    else if (address["town"]) {
+        document.getElementById("locInp").value = address["town"] + ', ' + address["state"];
+
+    }
+    else if (address["village"]) {
+        document.getElementById("locInp").value = address["village"] + ', ' + address["state"];
+
+    }
+      //document.getElementById("results").innerHTML = JSON.stringify(address);
+
+}
