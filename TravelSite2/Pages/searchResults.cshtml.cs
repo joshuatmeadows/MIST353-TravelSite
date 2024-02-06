99cc9aa23f137264dfb03a9cf3bbb16cdbc6606d
Joshua Meadows <jtmeadows@mail.wvu.edu>
2024-02-06 15:26:44 -0500

    class code 2/6

--- TravelSite2/Pages/searchResults.cshtml.cs
+++ TravelSite2/Pages/searchResults.cshtml.cs
@@ -5,8 +5,24 @@ namespace TravelSite2.Pages
 {
     public class searchResultsModel : PageModel
     {
-        public void OnGet()
+        private readonly IHttpClientFactory _clientFactory;
+        public string JsonData { get; set; }
+
+        public searchResultsModel(IHttpClientFactory clientFactory)
         {
+            _clientFactory = clientFactory;
         }
+        public async Task OnGetAsync()
+        {
+            var apiKey = "uZnzwcAwCLugMJGYsSejdFxmfhQWXTtI";
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://www.ncei.noaa.gov/cdo-web/api/v2/datasets");
+            request.Headers.Add("token", apiKey);
+
+            var client = _clientFactory.CreateClient();
+            var response = await client.SendAsync(request);
+
+            JsonData = await response.Content.ReadAsStringAsync();
+        }
+
     }
 }
